c4023048c7aabf448be9eb3b8c1d532b83a57207
AkmeemanaTD <99232376+AkmeemanaTD@users.noreply.github.com>
2024-09-17 19:41:17 +0530

    changes in model file

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['Admin', 'Manager', 'Employee'], default: 'Employee' },
+  role: { type: String, enum: ['Admin','admin','Manager','manager', 'Employee', 'employee'], default: 'Employee' },
 }, {
   timestamps: true,
 });
